f658956339753ea0786cc4b1f56d89e0aec05cc1
Shivam Saxena <91121317+shivamsaxena99@users.noreply.github.com>
2024-01-10 20:49:30 +0530

    Update Sendmail.php

--- Sendmail.php
+++ Sendmail.php
@@ -45,15 +45,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //Server settings
         $mail->SMTPDebug = 2;                                       //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+        $mail->Host       = '';                                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'shivamsaxena8200@gmail.com';           //SMTP username
-        $mail->Password   = 'ftdn hbnv lsrf jfva';                  //SMTP password
-        $mail->SMTPSecure = 'tls';                                  //Enable implicit TLS encryption
+        $mail->Username   = '';                                     //SMTP username
+        $mail->Password   = '';                                     //SMTP password
+        $mail->SMTPSecure = '';                                     //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('shivamsaxena8200@gmail.com', 'Shivam Saxena');
+        $mail->setFrom('', '');
         //$mail->addAddress('joe@example.net', 'Joe User');                   //Add a recipient
         $mail->addAddress($to_email);                                         //Name is optional
         //$mail->addAddress('ellen@example.com');                             //Name is optional
